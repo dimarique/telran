adb25b2484f7a4eb073d75e2b43c8b2f44f154f7
dimarique <pochtagreen@gmail.com>
2025-03-12 17:06:53 +0100

    add more lessons

---
+++ lesson10/scripts/script.js
@@ -0,0 +1,8 @@
+let r = 12
+let g = 34
+let b = 14
+let sym1 = Symbol('id')
+let sym2 = Symbol('id')
+console.log(`rgb(${r}, ${g}, ${b})`);
+console.log('rgb(' + r + ', ' + g + ', ' + b + ')')
+console.log(sym1 == sym2)
---
+++ lesson11/scripts/script.js
@@ -0,0 +1,11 @@
+//const age = Number(prompt('Enter your age'));
+
+typeof (age) !== 'number' || isNaN(age)
+	? console.log('Please enter number')
+	: console.log(`You are ${age} years alt`)
+console.log(new Date().getFullYear())
+const arr = [1, 4, 2, 5, 3];
+const squarredArr = arr.map(e => {
+	return squared = e ** 2
+});
+console.log(squarredArr)
